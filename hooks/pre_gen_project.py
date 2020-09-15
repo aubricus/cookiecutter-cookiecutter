5fc7c4ce652128f18a2e808931287dddae656ef0
Aubrey Taylor <aubricus@gmail.com>
2020-09-14 19:30:19 -0700

    Remove copy_hook, pre gen action

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,51 +3,10 @@
 See: https://cookiecutter.readthedocs.io/en/1.7.2/advanced/hooks.html
 """
 
-import os
-import shutil
-import fileinput
-from pathlib import Path
 
-
-# [Remove]
-# NOTE: The above directive is used to remove this section during copy_hook
-def copy_hook():
-    """Copy this repo's pre_gen_project hook to the template."""
-    src = Path(__file__)
-    dest = Path.cwd().joinpath("hooks/pre_gen_project.py")
-
-    try:
-        os.makedirs(str(dest.parent))
-        shutil.copy(str(src), str(dest))
-    except FileExistsError:
-        shutil.copy(str(src), str(dest))
-
-    _do_post_copy_remove(dest)
-
-
-def _do_post_copy_remove(file_path):
-    remove_token = "# [Remove]"
-    remove_closing_token = "# [End Remove]"
-    skip_line = False
-    with fileinput.FileInput(str(file_path), inplace=True) as f:
-        for line in f:
-            line = line.strip("\n")
-            if line.strip() == remove_token:
-                skip_line = True
-
-            if not skip_line:
-                print(line)
-
-            if line.strip() == remove_closing_token:
-                skip_line = False
-
-
-# [End Remove]
 def run():
     """Run pre gen hook functions."""
-    # [Remove]
-    copy_hook()
-    # [End Remove]
+    pass
 
 
 # NOTE: Do not delete this or the hook will not run.
